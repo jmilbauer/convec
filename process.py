283d51f9352c5fd6fdd0fac9d4c46a7349ef985b
Jay Dhanoa <jsdhanoa987@gmail.com>
2018-06-03 17:22:07 -0500

    added link parsing thing

---
+++ process.py
@@ -0,0 +1,14 @@
+import re
+
+link_pattern = r'(\[\[([A-z ]*?)(\|[A-z ]*?)?\]\])'
+
+# [[article link|hyperlink text]]
+# match.group(0) returns whole thing
+# match.group(1) returns article link
+# match.group(2) returns hyperlink text
+
+def return_links(text):
+    results = []
+    for match in re.finditer(link_pattern, text):
+        results.append(match.group(1))
+    return results
