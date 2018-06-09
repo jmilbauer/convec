e29eed307d311f40784257fb6df5e2f7b7c064bd
tophatraptor <jsdhanoa987@gmail.com>
2018-06-09 10:40:36 -0500

    updates to parsing functions, added data folder, added latest partial_xml file, created learning folder

--- tosentences.py
+++ tosentences.py
@@ -4,14 +4,15 @@ import os
 import sys
 import time
 import re
+import string
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-DATA_PATH = './data/'
-TEXT_PATH = './text/'
-TEXT_FOLDER = 'extracted'
-SENTENCES = 'sentences.txt'
-LABELS = 'labels.txt'
+DATA_PATH = 'data/'
+TEXT_PATH = 'text/'
+TEXT_FOLDER = 'ld1_extracted'
+SENTENCES = 'ld1_sentences.txt'
+LABELS = 'ld1_labels.txt'
 ENCODING = "utf-8"
 
 
@@ -23,7 +24,6 @@ interior_pattern = r"(?s)<doc .*?title=\"(.*?)\">(.*?)<\/doc>"
 
 data = []
 files = [os.path.join(dp, f) for dp, dn, fn in os.walk(extractedDir) for f in fn if not f.startswith('.')]
-
 counter = 0
 for filepath in files:
 
@@ -38,12 +38,22 @@ for filepath in files:
         for sentence in sentences:
             data.append((sentence, title))
 
+def clean_sentence(text):
+    """Removes punctuation"""
+    text = re.sub(r'([A-Za-z])\.([A-Za-z])', r'\1\2', text)
+    text = re.sub(r'([A-Za-z ])\.\s*', r'\1 ', text.strip())
+    text = re.sub(r'\.$', '', text)
+    text = re.sub(r'[^\w\s\.\-]', r'', text)
+    text = text.lower()
+    text = re.sub(r'[0-9]+', 'NUM', text)
+    return text
 
 with open(sentencePath, 'wb+') as sentence_fp:
     with open(labelPath, 'wb+') as label_fp:
         for (s, l) in data:
-            sentence_fp.write(s)
+
+            sentence_fp.write(clean_sentence(s))
             sentence_fp.write('\n')
 
-            label_fp.write(l)
+            label_fp.write(l.lower().replace(' ', '_'))
             label_fp.write('\n')
